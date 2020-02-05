3b1bd7734262e3875bccbfd22a16c60f3b884679
faisalahmedbd <faisalcse29@gmail.com>
2020-02-05 19:30:49 +0600

    init calendar

--- src/components/calendar.js
+++ src/components/calendar.js
@@ -7,11 +7,13 @@ export default class Calendar extends React.Component {
         super(props);
         this.state = {
             month: moment().month(),
+            startWeek: moment().startOf('month').week(),
+            endWeek: moment().endOf('month').week()
         }
     }
     renderCalendar = () => {
-        const { month } = this.state;
-        const startWeek = moment().startOf('month').week(), endWeek = moment().endOf('month').week();
+        const { month, startWeek, endWeek } = this.state;
+        // const startWeek = moment().startOf('month').week(), endWeek = moment().endOf('month').week();
         let weeks = [];
         for (let week = startWeek; week <= endWeek; week++) {
             let startingDate = moment().week(week).startOf('week')
@@ -23,14 +25,26 @@ export default class Calendar extends React.Component {
         }
         return weeks;
     }
+
+    handleMonthChange = (type) => {
+        const { month, startWeek, endWeek } = this.state;
+        this.setState({
+            month: type === 'prev' ? month - 1 : month + 1,
+            startWeek: type === 'prev' ? startWeek - 1 : endWeek + 1,
+            endWeek: type === 'prev' ? startWeek - 5 : endWeek + 5
+        })
+    }
+
     render() {
         const { month } = this.state;
         return (
             <div className={`calendar`}>
                 <div className="calendar-caption">
                     <h1>{MONTHS[month]}</h1>
-                    <button>prev</button>
-                    <button onClick={() => this.setState({ month: month + 1 })}>next</button>
+                    {/* <button onClick={() => this.setState({ month: month - 1 })}>prev</button>
+                    <button onClick={() => this.setState({ month: month + 1 })}>next</button> */}
+                    <button onClick={() => this.handleMonthChange('prev')}>prev</button>
+                    <button onClick={() => this.handleMonthChange('next')}>next</button>
                 </div>
 
                 <div className="week-days-name week-row">
